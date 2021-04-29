fc0b8ba1ac33f7d78a606cee30773288e6c7538b
JustinyAhin <justiny.ahinon@gmail.com>
2021-04-29 11:24:53 +0100

    Test if the Yoast SEO plugin metabox is present on a post edit page

---
+++ packages/e2e-tests/specs/check-yoast-seo-metabox.test.js
@@ -0,0 +1,25 @@
+/**
+ * WordPress e2e utilities
+ */
+import {
+	createNewPost,
+	installPlugin,
+} from '@wordpress/e2e-test-utils';
+
+describe( 'Edit Posts', () => {
+	beforeEach( async () => {
+		// Testing this with Yoast plugin installed and activated
+		// on my WordPress install  (wp-env start run from the plugin folder)
+
+		// await installPlugin( 'wordpress-seo' );
+	} );
+
+	it( 'should contains the Yoast SEO plugin metabox', async () => {
+		await createNewPost();
+
+		const yoastSeoMetabox = await page.$x(
+			`//*[contains(@class, 'postbox yoast wpseo-metabox')]`
+		);
+		expect( yoastSeoMetabox.length ).toBe( 1 );
+	} );
+} );
